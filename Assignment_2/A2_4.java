d822199c23ed66af71c21793e8fa1dcba1bbf1dc
CODER <125824567+DEVIL001-CODER@users.noreply.github.com>
2024-02-24 16:16:45 +0530

    Add files via upload

---
+++ Assignment_2/A2_4.java
@@ -0,0 +1,42 @@
+
+import java.util.Scanner;
+
+public class A2_4 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("ENTER TOTAL NUMBERS TO BE CHECKED -->");
+        int n = in.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("ENTER NUMBER LIST -->");
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = in.nextInt();
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                    i = j;
+                }
+            }
+            System.out.println("OCCURENCE OF " + arr[i] + " IN THE GIVEN NUMBER LIST --> " + count);
+        }
+
+        in.close();
+    }
+}
---
+++ Assignment_2/A2_5.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+public class A2_5 {
+    public static void main(String args[]) {
+        int array[][] = new int[3][3];
+        System.out.println("Enter 9 elements of the 3*3 matrix:");
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                array[i][j] = sc.nextInt();
+            }
+        }
+        int left = 0, right = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == j) {
+                    left += array[i][j];
+                }
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 2; j >= 0; j--) {
+                if ((i == j) && (i != 0) && (i != 2) || (i - j) == 2 || (i - j) == -2) {
+                    right += array[i][j];
+                }
+            }
+        }
+        System.out.println("Sum of left diagonal elements: " + left);
+        System.out.println("Sum of right diagonal elements: " + right);
+        sc.close();
+    }
+}
